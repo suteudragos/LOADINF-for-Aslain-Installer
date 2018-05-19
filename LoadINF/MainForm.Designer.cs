1bd3e9c4e580b2461473b180879bcb0d1a870dde
Dragos Suteu <rmxshoot@gmail.com>
2018-05-19 23:03:05 +0300

    Disabled Maximize box

--- LoadINF/MainForm.Designer.cs
+++ LoadINF/MainForm.Designer.cs
@@ -186,6 +186,7 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LOADINF for Aslain\'s Installer";
